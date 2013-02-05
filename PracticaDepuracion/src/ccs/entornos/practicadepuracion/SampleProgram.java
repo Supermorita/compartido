68a92ccc2b75d3fcc5e4066592969bd6328d3b0d
Tarde <ruben_novella@hotmail.com>
2013-02-05 18:12:29 +0100

    ProyectoDepuracion inicial

---
+++ PracticaDepuracion/src/ccs/entornos/practicadepuracion/SampleProgram.java
@@ -0,0 +1,28 @@
+package ccs.entornos.practicadepuracion;
+public class SampleProgram {
+    
+    public static void main(String[] args) {
+        System.out.println("Starting...");
+        
+        for (int i=0; i<10; i++) {
+            innerLoop(i);
+        }
+
+        System.out.println("Finished.");
+    }
+
+    public static void innerLoop(int i){
+        for (int j = 0; j < 10000; j++) {
+   
+        	
+        	System.out.println("i=" + i + ", j=" + j);
+            delay();
+        }
+    }
+    
+    //Adapta la velocidad a la de tu ordenador
+    public static void delay(){
+        for (int k = 0; k < 1000; k++)
+            ; //estamento vacio
+    }
+}
